2a3bb34bb6e7b76b841962c654a4978b426bf356
Blake Billmaier <blakebillmaier@gmail.com>
2017-01-01 19:52:45 -0500

    First commit of 2017. Added reddit support to magic mirror

--- app/app.js
+++ app/app.js
@@ -45,11 +45,11 @@ $( document ).ready(function() {
 
 
     var googleAPIKey = 'AIzaSyAhCLSHKTqG9rN9TWyK7RJopJoAQRO7yH0';
-    var startLocation = '235 Fort Pitt Blvd Pittsburgh';
-    var destination = '233 N Craig Street Pittsburgh';
+    var startLocation = '233 N Craig Street Pittsburgh';
+    var destination = '235 Fort Pitt Blvd Pittsburgh';
 
-    getTraffic(startLocation, destination, googleAPIKey, false, 12, 23);
-    setInterval(function(){ getTraffic(startLocation, destination, googleAPIKey, false, 8, 10); }, 6000);
+    getTraffic(startLocation, destination, googleAPIKey, false, 0, 23);
+    setInterval(function(){ getTraffic(startLocation, destination, googleAPIKey, false, 0, 23); }, 6000);
     
     getCurrentWeather('Pittsburgh', 'imperial');
     setInterval(function(){ 
@@ -64,8 +64,10 @@ $( document ).ready(function() {
       //console.log('date updated');
     }, 1000);
     
-    getNewsFeed('techcrunch', '050ace9a6f81445a9e0f9d0ee68a7e0b');
- 
+    //getNewsFeed('techcrunch', '050ace9a6f81445a9e0f9d0ee68a7e0b');
+    
+    getRedditFeed('news', 'hot', 5);
+    setInterval(function(){ getRedditFeed('news', 'hot', 5); }, 60000);
 });
 
 function startTime() {
@@ -178,25 +180,42 @@ function theDate(){
 	return today;
 }
 
-function getNewsFeed(source, APIkey){
-  var newsQuery = 'https://newsapi.org/v1/articles?source=' + source + '&apiKey=' + APIkey;
-  var newsRequest = $.ajax({
-      url: newsQuery,
+function getRedditFeed(subreddit, sort, count){
+  $('#subreddit').html('');
+  $('#subreddit').append(subreddit);
+  if(sort){
+    //Do Nothing
+  }else{
+    sort = 'hot';
+  }
+  
+  if(count){
+    //Do Nothing
+  }else{
+    count = 20;
+  }
+  console.log('Reddit Count is ' + count);
+  var redditQuery = 'https://www.reddit.com/r/' + subreddit + '/' + sort + '/.json?count=' + count;
+  console.log(redditQuery);
+  var redditRequest = $.ajax({
+      url: redditQuery,
       method: "GET",
     });
+  
+    redditRequest.done(function( msg ) {
+      $("#reddit-container").html('');
+      console.log('Reddit: ');
+      //console.log(msg.data.children);
+      var r = 0;
+      
+      while(r < count){
+        console.log(msg.data.children[r]);
+        var thisPost = msg.data.children[r].data;
 
+        $("#reddit-container").append('<div class="reddit-single"><div class="title">' + thisPost.title + '</div><div class="domain">' + thisPost.domain + '</div><div class="author">u/' + thisPost.author + '</div></div>');        
+        r++;
+      }
 
-    newsRequest.done(function( msg ) {
-      console.log('News: ');
-      console.log(msg);
-
-      var i = 0;
-      msg.articles.forEach(function(){
-        var thisArticle = msg.articles[i];
-        $("#news-container").append('<div class="news-single"> <h2 class="headline">' + thisArticle.title + '</h2> <div class="by-line"><span class="source">' + source + '</span> - <span class="author">' + thisArticle.author + '</span></div> </div>');
-        console.log(thisArticle.title);
-        i++;
-      });
     });
 }
 
--- src/app.js
+++ src/app.js
@@ -41,8 +41,8 @@ $( document ).ready(function() {
     var startLocation = '233 N Craig Street Pittsburgh';
     var destination = '235 Fort Pitt Blvd Pittsburgh';
 
-    getTraffic(startLocation, destination, googleAPIKey, false, 8, 10);
-    setInterval(function(){ getTraffic(startLocation, destination, googleAPIKey, false, 8, 10); }, 6000);
+    getTraffic(startLocation, destination, googleAPIKey, false, 0, 23);
+    setInterval(function(){ getTraffic(startLocation, destination, googleAPIKey, false, 0, 23); }, 6000);
     
     getCurrentWeather('Pittsburgh', 'imperial');
     setInterval(function(){ 
@@ -57,8 +57,10 @@ $( document ).ready(function() {
       //console.log('date updated');
     }, 1000);
     
-    getNewsFeed('techcrunch', '050ace9a6f81445a9e0f9d0ee68a7e0b');
- 
+    //getNewsFeed('techcrunch', '050ace9a6f81445a9e0f9d0ee68a7e0b');
+    
+    getRedditFeed('news', 'hot', 5);
+    setInterval(function(){ getRedditFeed('news', 'hot', 5); }, 60000);
 });
 
 function startTime() {
@@ -206,5 +208,44 @@ function getNewsFeed(source, APIkey){
         console.log(thisArticle.title);
         i++;
       });
+    });
+}
+
+function getRedditFeed(subreddit, sort, count){
+  $('#subreddit').html('');
+  $('#subreddit').append(subreddit);
+  if(sort){
+    //Do Nothing
+  }else{
+    sort = 'hot';
+  }
+  
+  if(count){
+    //Do Nothing
+  }else{
+    count = 20;
+  }
+  console.log('Reddit Count is ' + count);
+  var redditQuery = 'https://www.reddit.com/r/' + subreddit + '/' + sort + '/.json?count=' + count;
+  console.log(redditQuery);
+  var redditRequest = $.ajax({
+      url: redditQuery,
+      method: "GET",
+    });
+  
+    redditRequest.done(function( msg ) {
+      $("#reddit-container").html('');
+      console.log('Reddit: ');
+      //console.log(msg.data.children);
+      var r = 0;
+      
+      while(r < count){
+        console.log(msg.data.children[r]);
+        var thisPost = msg.data.children[r].data;
+
+        $("#reddit-container").append('<div class="reddit-single"><div class="title">' + thisPost.title + '</div><div class="domain">' + thisPost.domain + '</div><div class="author">u/' + thisPost.author + '</div></div>');        
+        r++;
+      }
+
     });
 }
